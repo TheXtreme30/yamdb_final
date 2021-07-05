4fdd2e7b6d61a9c38362ce967d15c327f6042aa3
Sergey <thextreme.30@gmail.com>
2021-07-05 18:22:30 +0600

    fix allowed hosts

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -10,10 +10,7 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    os.environ.get('DOMAIN_NAME'),
-    os.environ.get('HOST'),
-]
+ALLOWED_HOSTS = ['*']
 
 
 INSTALLED_APPS = [
